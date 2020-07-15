85f08f8468775c8b0ebf2d55e5b481e0aa2abc65
hjhearts <60081459+hjhearts@users.noreply.github.com>
2020-07-15 17:35:00 +0900

    MVC, Repository, Test

--- src/main/java/com/web/service/BoardService.java
+++ src/main/java/com/web/service/BoardService.java
@@ -2,6 +2,7 @@ package com.web.service;
 
 import com.web.domain.Board;
 import com.web.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
